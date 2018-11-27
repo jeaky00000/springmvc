cc3cae430183f7dd9c55a74a2e4171efa13a9adb
zhaitb <zhaitb@fxtcn.com>
2018-11-27 18:12:01 +0800

    config file and annotation

--- src/main/java/com/dabao/controllers/UserController.java
+++ src/main/java/com/dabao/controllers/UserController.java
@@ -1,16 +1,24 @@
 package com.dabao.controllers;
 
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.ServletRequestDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Controller
 @RequestMapping("/user")
 @ResponseBody
 public class UserController {
     @GetMapping("/list")
-    public String list(){
-        return "hello list";
+    public String list(@RequestParam String name){
+        return "hello list" + name;
+    }
+
+    @InitBinder
+    public void initBinder(ServletRequestDataBinder binder){
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(new SimpleDateFormat("yyyy-MM-dd"), true));
     }
 }
